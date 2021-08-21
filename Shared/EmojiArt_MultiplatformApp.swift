c333e8a318b456f34b5a0c2ec8d4e41d76d2cdeb
Jizheng Yang <jy1700@nyu.edu>
2021-08-21 10:29:45 -0400

    Initial Commit

---
+++ Shared/EmojiArt_MultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArt_MultiplatformApp.swift
+//  Shared
+//
+//  Created by Jizheng Yang on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArt_MultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
